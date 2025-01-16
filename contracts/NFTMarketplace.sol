d667ad44a9692c5ed8ce40bc349fd9d5e05e4396
Gabriel Kassarp <gkassarp@gmail.com>
2025-01-16 14:13:55 +0100

    written up basic struct and mapping for nft listing

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -5,3 +5,39 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+contract NFTMarketplace is ERC721URIStorage {
+
+
+address payable owner;
+
+using Counters for Counters.Counter;
+Counters.Counter private _tokenIds;
+Counters.Counter private _itemsSold;
+
+uint256 listPrice = 0.001 ether;
+
+    constructor () ERC721("NFTMarketplace", "NFTM") {
+
+        owner = payable(msg.sender);
+    }
+
+
+
+struct ListedToken {
+    uint256 tokenId;
+    address payable owner;
+    address payable seller;
+    uint256 price;
+    bool currentlyListed;
+}
+
+mapping(uint256 => ListedToken) private idToListedToken;
+
+function updateListPrice(uint256 _listPrice) public payable {
+    require(owner == msg.sender, "Only the owner can update the list price");
+    listPrice = _listPrice;
+}
+
+}

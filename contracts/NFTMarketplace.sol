7b4dc00a49520847aefa57571905fef465cf502b
Gabriel Kassarp <gkassarp@gmail.com>
2025-01-16 16:04:50 +0100

    added main functions to solidity contract

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -16,7 +16,7 @@ using Counters for Counters.Counter;
 Counters.Counter private _tokenIds;
 Counters.Counter private _itemsSold;
 
-uint256 listPrice = 0.001 ether;
+uint256 listPrice = 0.0001 ether;
 
     constructor () ERC721("NFTMarketplace", "NFTM") {
 
@@ -56,4 +56,101 @@ function getListedForTokenId(uint256 tokenId) public view returns (ListedToken m
 
 function getCurrentTokenId() public view returns (uint256) {
     return _tokenIds.current();
+}
+
+function createToken(string memory tokenURI, uint256 price) 
+public payable returns (uint){
+require (msg.value == listPrice, "Please pay the list price to create a token");
+require (price > 0, "Price must be greater than 0");
+
+_tokenIds.increment();
+uint256 currentTokenId = _tokenIds.current();
+_safeMint(msg.sender, currentTokenId);
+
+_setTokenURI(currentTokenId, tokenURI);
+
+createListedToken(currentTokenId, price);
+
+return currentTokenId;
+}
+
+function createListedToken(uint256 tokenId, uint256 price) private {
+    idToListedToken[tokenId] = ListedToken(tokenId,
+    payable (address(this)), 
+    payable(msg.sender),
+    price,
+    true
+      );
+ _transfer(msg.sender, address(this), tokenId);
+}
+
+function getAllNFTs() public view returns (ListedToken[] memory) {
+    uint nftCount = _tokenIds.current();
+    ListedToken[] memory tokens = new ListedToken[](nftCount);
+
+
+    uint currentIndex = 0;
+
+    for (uint i = 1; i <= nftCount; i++) {
+        uint currentId = i+1;
+        ListedToken storage currentItem = idToListedToken[currentId];
+        tokens[currentIndex] = currentItem;
+        currentIndex += 1;
+        }
+
+        return tokens;
+    }
+
+    function getMyNFTs() public view returns (ListedToken[] memory) {
+
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0; 
+        uint currentIndex = 0;
+
+        for (uint i=0; i < totalItemCount; i++) {
+        
+        if(idToListedToken[i+1].owner == msg.sender || idToListedToken[i+1].seller == msg.sender) {
+            itemCount += 1;
+        }
+}
+
+
+ListedToken[] memory items = new ListedToken[](itemCount);
+for (uint i=0; i < totalItemCount; i++) {
+    if(idToListedToken[i+1].owner == msg.sender || idToListedToken[i+1].seller == msg.sender) {
+        uint currentId = i+1;
+        ListedToken storage currentItem = idToListedToken[currentId];
+        items[currentIndex] = currentItem;
+        currentIndex += 1;
+    }
+ }
+
+ return items;
+
+    }
+
+    function executeSale(uint256 tokenId) public payable {
+
+        uint price = idToListedToken[tokenId].price;
+        require(msg.value == price, "Please pay the asking price to execute the sale");
+
+
+        address seller = idToListedToken[tokenId].seller;
+       
+       
+        idToLIstedToken[tokenId].currentlyListed = true;
+        idToLIstedToken[tokenId].seller = payable(msg.sender);
+        _itemsSold.increment();
+
+
+
+        _payable(address(this), msg.sender, tokenId);
+
+        approve(address(this), tokenId);
+
+        payable(owner).transfer(listPrice);
+        payable(seller).transfer(msg.value);
+
+    }
+
 }
